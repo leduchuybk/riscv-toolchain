ee2819c0b8bbee9ab322d8dffb20cb712a76ddd4
leduchuybk <leduchuybk@leduchuybk@gmail.com>
2021-09-13 13:29:42 +0900

    Init project

---
+++ lib/gcc/riscv64-unknown-elf/10.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/build/src/sc-ide.200629/riscv-gnu-toolchain/riscv-gcc/configure --target=riscv64-unknown-elf --prefix=/opt/syntacore/riscv-gcc-10.2.0-gb693c14--200904T1226 --disable-shared --disable-threads --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/opt/syntacore/riscv-gcc-10.2.0-gb693c14--200904T1226/riscv64-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=/home/build/src/sc-ide.200629/riscv-gnu-toolchain/riscv-gcc --enable-checking=no --enable-multilib --with-abi=lp64d --with-arch=rv64imafdc --with-tune=rocket 'CFLAGS_FOR_TARGET=-O2   -mcmodel=medany' 'CXXFLAGS_FOR_TARGET=-O2   -mcmodel=medany'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64imafdc" }, { "tune", "rocket" } };
---
+++ lib/gcc/riscv64-unknown-linux-gnu/10.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/build/src/sc-ide.200629/riscv-gnu-toolchain/riscv-gcc/configure --target=riscv64-unknown-linux-gnu --prefix=/opt/syntacore/riscv-gcc-10.2.0-gb693c14--200904T1226 --with-sysroot=/opt/syntacore/riscv-gcc-10.2.0-gb693c14--200904T1226/sysroot --with-system-zlib --enable-shared --enable-tls --enable-languages=c,c++,fortran --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libsanitizer --disable-nls --disable-bootstrap --src=/home/build/src/sc-ide.200629/riscv-gnu-toolchain/riscv-gcc --enable-checking=no --enable-multilib --with-abi=lp64d --with-arch=rv64imafdc --with-tune=rocket 'CFLAGS_FOR_TARGET=-O2   -mcmodel=medany' 'CXXFLAGS_FOR_TARGET=-O2   -mcmodel=medany'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64imafdc" }, { "tune", "rocket" } };
